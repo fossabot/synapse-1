b3e60d59086887f398968d52fd7c8e34f296e689
panev <hi@panev.net>
2017-12-02 15:50:48 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
-const square = d3.selectAll("rect");
+import 'mindex.js';
 
+const square = d3.selectAll("rect");
 
 square.style("fill", "orange");
 console.log("working");
---
+++ src/mindex.js
@@ -0,0 +1,2 @@
+
+console.log("double working");

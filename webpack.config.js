3f6be66dd839cdcf5f692ef9130b7599e62d4c00
panev <hi@panev.net>
2017-12-02 17:26:39 +0200

    comment cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,5 @@ module.exports = {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
   },
-  // module: {
-  //   loaders: [
-  //     { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-  //   ]
-  // },
   plugins: [HtmlWebpackPluginConfig]
 }

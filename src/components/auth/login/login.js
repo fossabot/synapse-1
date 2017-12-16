53e19213444cc565fbcb09a5f9c121647f9dbf8a
panev <hi@panev.net>
2017-12-16 20:18:31 +0200

    login base

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -1,3 +1,16 @@
 import './login.css';
 
-console.log("blah");
+// get form elements
+var formEmail = document.querySelector('.log-input-email');
+var formPassword = document.querySelector('.log-input-password');
+var formButtonLogin = document.querySelector('.log-in-button');
+
+formButtonLogin.addEventListener('click', e => {
+    var email = formEmail.value;
+    var pass = formPassword.value;
+    var auth = firebase.auth();
+
+    var promise = auth.signInWithEmailAndPassword(email, pass);
+
+    promise.catch(e => console.log(e.message));
+});
